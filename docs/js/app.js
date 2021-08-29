3e89052c47f874ba88569f8f9d9c2614c37e7b5e
Ankita <ankita@ajency.in>
2021-08-29 23:15:58 +0530

    build

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,28 +7,39 @@ $(document).ready(function(){
 
 		if($(this).hasClass('block1')){
 			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('expand');
-			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('swiper-slide-active');
-			$('.slider').find('.shelf-block--details .shelf-block--wrap:nth-child(1)').addClass('expand');
+			$('.slider').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('expand');
 		}
 		if($(this).hasClass('block2')){
 			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('expand');
-			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('swiper-slide-active');
-			$('.slider').find('.shelf-block--details .shelf-block--wrap:nth-child(2)').addClass('expand');
+			$('.slider').find('.shelf-block--details .shelf-block--wrap.wrap2').addClass('expand');
 		}
 		if($(this).hasClass('block3')){
 			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('expand');
-			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('swiper-slide-active');
-			$('.slider').find('.shelf-block--details .shelf-block--wrap:nth-child(3)').addClass('expand');
+			$('.slider').find('.shelf-block--details .shelf-block--wrap.wrap3').addClass('expand');
 		}
 		if($(this).hasClass('block4')){
 			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('expand');
-			$('.slider .shelf-block--details .shelf-block--wrap').removeClass('swiper-slide-active');
-			$('.slider').find('.shelf-block--details .shelf-block--wrap:nth-child(4)').addClass('expand');
+			$('.slider').find('.shelf-block--details .shelf-block--wrap.wrap4').addClass('expand');
 		}
 		$('.podium').addClass('podium-animate');
-   });
+   	});
 
-    new Swiper('.swiper-container', {
-        
-    });
+   
+	var slider = $('#slider').swipeSlider({
+
+		animationDuration: 300,
+		autoReverse: true,
+		autoTransitionDuration: false, // in ms
+		bounce: true,
+		drag: true,
+		infinite: true,
+		onSlideStartCallback: function() {},
+		onSlideCompleteCallback: function() {},
+		onMoveCallback: function() {},
+		onStartCallback: function() {},
+		startIndex: 0,
+		allowTouchMove: false
+	});
 });
+
+
