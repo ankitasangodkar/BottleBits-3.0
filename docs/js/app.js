c3de827ffc2347206d35c074367f13eb5873f809
Ankita <ankita@ajency.in>
2021-08-30 11:01:37 +0530

    fixes

--- docs/js/app.js
+++ docs/js/app.js
@@ -7,16 +7,24 @@ $(document).ready(function(){
 		$(".carousel .shelf-block .shelf-block--wrap").addClass('shelf-block--slider');
 
 		if($(this).hasClass('block1')){
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').prev().addClass('prev');
 			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').next().addClass('next');
 		}
 		if($(this).hasClass('block2')){
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').prev().addClass('prev');
 			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap2').next().addClass('next');
 		}
 		if($(this).hasClass('block3')){
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').prev().addClass('prev');
 			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap3').next().addClass('next');
 		}
 		if($(this).hasClass('block4')){
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap4').prev().addClass('prev');
 			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap4').addClass('active');
+			$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('next');
 		}
 		$('.podium').addClass('podium-animate');
    	});
@@ -36,12 +44,16 @@ var $carousel = $('.carousel'),
     $carouselItems = $('.carousel-item', $carousel);
 
 //This event is fired when the carousel has completed its slide transition.
-$carousel.on('slid.bs.carousel', function (e) {
+	$carousel.on('slid.bs.carousel', function (e) {
     //Reset classes
     $carouselItems.removeClass('prev next');
     //Find current slide
     var $active = $(e.relatedTarget);
+    if(($active).hasClass('wrap4')){
+    	$('.carousel').find('.shelf-block--details .shelf-block--wrap.wrap1').addClass('next');
+    }else{
     //Add corresponding classes to next and prev elements
-    $active.next().addClass('next');
+    	$active.next().addClass('next');
+	}
     $active.prev().addClass('prev');
 });
